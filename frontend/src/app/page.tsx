9921723e5659a96b0a4e60027f952641a210a0c5
Seiya Nishimori <s-nishimori@bloomclassic.jp>
2026-03-18 18:59:32 +0900

    chore: Next.jsを現時点安定板15.1.0に固定し環境構築完了（Next.js16の最新版のバグが確認された為）

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,43 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function Home() {
-    const [message, setMessage] = useState("バックエンドに接続中...");
 
-    useEffect(() => {
-        fetch("http://localhost:8000/")
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message))
-            .catch((err) => {
-                console.error("通信エラー:", err);
-                setMessage("バックエンドから返事がありません");
-            });
-    }, []);
-
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-gray-100">
-            <div className="p-10 bg-white rounded-2xl shadow-xl text-center">
-                <h1 className="text-2xl font-bold text-gray-800 mb-4">
-                    勤怠管理システム 構築テスト
-                </h1>
-
-                {/* ここでＨｅｌｌｏを呼び出す */}
-                <Hello />
-
-
-                <div className="text-xl font-mono text-blue-600 bg-blue-50 p-4 rounded-lg border border-blue-200">
-                    {message}
-                </div>
-
-                <HTReload />
-
-            </div>
-
-
-        </div>
-    );
-
-}
 
 
 //Hello コンポーネント
@@ -53,3 +17,82 @@ function Hello() {
 function HTReload() {
     return <h1 className="text-2xl text-green-500">ホットリロード動け！！</h1>;
 }
+
+
+
+export default function Home() {
+    const [message, setMessage] = useState("バックエンドに接続中...");
+
+    let str: string = 'Hello World';
+    console.log("Hello, TypeScript!");
+
+    console.log("Hello, TypeScript!  2");
+
+    useEffect(() => {
+
+        console.log("Hello");
+
+        fetch("http://localhost:8000/")
+            .then((res) => res.json())
+            .then((data) => setMessage(data.message))
+            .catch((err) => {
+                console.error("通信エラー:", err);
+                setMessage("バックエンドから返事がありません");
+            });
+    }, []);
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gray-100">
+            <div className="p-10 bg-white rounded-2xl shadow-xl text-center">
+                <h1 className="text-2xl font-bold text-gray-800 mb-4">
+                    勤怠管理システム 構築テストqqqqqq
+                </h1>
+
+                {/* ここでＨｅｌｌｏを呼び出す（関数呼び出し） */}
+                <Hello />
+
+
+                <div className="text-xl font-mono text-blue-600 bg-blue-50 p-4 rounded-lg border border-blue-200">
+                    {message}
+                </div>
+
+                {/* （関数呼び出し） */}
+                <HTReload />
+
+
+                何でもいいので文字を変えて保存してみる
+                // console.log("★★★ ログテスト中 ★★★");eeeeee
+
+                console.log("★★★ ログテスト中 ★★★");
+
+            </div>
+
+
+        </div>
+    );
+
+}
+
+
+// // 何でもいいので文字を変えて保存してみる
+// console.log("★★★ ログテスト中 ★★★");
+
+
+
+// // 何でもいいので文字を変えて保存してみる
+// console.log("★★★ ログテスト中 ★★★");
+
+
+
+// // 何でもいいので文字を変えて保存してみる
+console.log("★★★ ログテスト中 ★★★");
+
+
+
+
+
+
+
+
+
+
